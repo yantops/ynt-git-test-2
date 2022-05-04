8c43b4c29f60cc221697a8c45f0946878030fff1
yantops <dharmawanriyanto@gmail.com>
2022-05-04 15:15:32 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello UFO!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat laboriosam repellat ipsa porro, maxime accusamus nostrum neque saepe consectetur labore quos quisquam, totam facilis, quis dolore aliquid. Placeat, qui voluptatum.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
